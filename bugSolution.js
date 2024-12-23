6a9f9b28b1e4891466b16c1cf88ee9fe91903388
BugHunter-X <askzenai@gmail.com>
2024-12-23 01:44:07 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,35 @@
+```javascript
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route path="nested" element={<Nested />} />
+        </Route>
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return <div>Home</div>;
+}
+
+function About() {
+  return <div>About</div>;
+}
+
+function NotFound() {
+  return <div>Not Found</div>;
+}
+
+function Nested(){
+  return <div>Nested</div>;
+}
+
+export default App;
+```
